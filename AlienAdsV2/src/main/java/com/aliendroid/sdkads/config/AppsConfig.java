123991149cd57ad91612c700db936b80be59dccf
reskin-aliendroid <rysasahrial@aliendro.id>
2024-07-05 09:37:45 +0700

    gameKey

---
+++ AlienAdsV2/src/main/java/com/aliendroid/sdkads/config/AppsConfig.java
@@ -0,0 +1,6 @@
+package com.aliendroid.sdkads.config;
+
+public class AppsConfig {
+    public static String GAMEKEY ="T7PPns0K6JV00uGv0ZAEKsTWrpwA-N4Hchi_KKecaqTa_U5zQcyyoI_pTcC5TM1OgfrLz5dWGdASKWgK6l5Sks";
+
+}
